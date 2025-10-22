6f9e8372c9fe0aae39eb8d11bee4542287d1adde
Michael Madden <mikemadden42@icloud.com>
2025-10-22 09:58:42 -0500

    Add parameter for copies.

--- cmd/books/main.go
+++ cmd/books/main.go
@@ -2,12 +2,13 @@ package main
 
 import "fmt"
 
-func printBook(title, author string) {
-	fmt.Println(title, "by", author)
+func printBook(title, author string, copies int) {
+	fmt.Println(title, "by", author, "-", copies, "copies")
 }
 func main() {
 	fmt.Println("Books in stock:")
 	title := "The Deeper Love of Go"
 	author := "John Arundel"
-	printBook(title, author)
+	copies := 15
+	printBook(title, author, copies)
 }
