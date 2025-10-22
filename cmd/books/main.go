339b9a3ea5f908ebb224937d3f1a7a50bd29e3bc
Michael Madden <mikemadden42@icloud.com>
2025-10-22 09:38:47 -0500

    Add printBook function.

--- cmd/books/main.go
+++ cmd/books/main.go
@@ -2,9 +2,12 @@ package main
 
 import "fmt"
 
+func printBook(title, author string) {
+	fmt.Println(title, "by", author)
+}
 func main() {
 	fmt.Println("Books in stock:")
 	title := "The Deeper Love of Go"
 	author := "John Arundel"
-	fmt.Println(title, "by", author)
+	printBook(title, author)
 }
